2827660bc118d66579fa852d276a60bfc260507d
krzygorz <krzygorz@gmail.com>
2019-03-16 19:34:25 +0100

    Fix 'operations' key in config, add operation descriptions to README

--- mapmanager/exe_main.py
+++ mapmanager/exe_main.py
@@ -5,13 +5,16 @@ from mapmanager import cli
 from configparser import ConfigParser, NoSectionError
 
 cfg = ConfigParser() #TODO: move this to cli.py
+#TODO: JSON might be a better choice
 
 try:
     cfg.read('config.cfg')
     args = dict(cfg.items('args'))
 except NoSectionError:
     args = {}
-    
+
+args['operations'] = args['operations'].split()
+
 try:
     cli.main(args)
 except Exception:
